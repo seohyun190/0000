9abfe42803ded94836cec569c89c6f4673a499d4
seohyun190 <seoseohyun0223@gmail.com>
2025-09-30 23:27:00 +0900

    2차

--- a.c
+++ a.c
@@ -13,3 +13,38 @@ int main(void) {
 
     return 0;
 }
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= 9; i++) {
+        printf("%d * %d = %d\n", N, i, N * i);
+    }
+
+    return 0;
+}
+#include <stdio.h>
+
+int main(void) {
+    int X; // 총 금액
+    int N; // 구매한 물건 종류 수
+    int a, b; // 물건 가격, 개수
+    int sum = 0;
+
+    scanf("%d", &X);
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &a, &b);
+        sum += a * b;
+    }
+
+    if (sum == X)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
